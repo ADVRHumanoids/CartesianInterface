db07d3635cfb207106484ef2911f9cfa2eedd7e0
EnricoMingo <enricomingo@gmail.com>
2019-02-12 12:27:43 +0100

    small fix

--- src/problem/Cartesian.cpp
+++ src/problem/Cartesian.cpp
@@ -42,9 +42,9 @@ TaskDescription::Ptr CartesianTask::yaml_parse_cartesian(YAML::Node task_node, M
         cart_task->orientation_gain = task_node["orientation_gain"].as<double>();
     }
 
-    if(task_node["is_body_jacobian"])
+    if(task_node["body_jacobian"])
     {
-        cart_task->is_body_jacobian = task_node["is_body_jacobian"].as<bool>();
+        cart_task->is_body_jacobian = task_node["body_jacobian"].as<bool>();
     }
 
     return task_desc;
