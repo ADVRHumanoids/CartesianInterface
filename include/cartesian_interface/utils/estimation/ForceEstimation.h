347c0707366a345e640d0d462c05a0a460801615
Arturo Laurenzi <arturo.laurenzi@iit.it>
2019-11-20 10:21:31 +0100

    Adding doc to force estimation

--- include/cartesian_interface/utils/estimation/ForceEstimation.h
+++ include/cartesian_interface/utils/estimation/ForceEstimation.h
@@ -5,59 +5,76 @@
 #include <algorithm>
 
 namespace XBot { namespace Cartesian { namespace Utils {
-    
-    class ForceEstimation
-    {
-      
-    public:
-        
-        typedef std::shared_ptr<ForceEstimation> Ptr;
-        
-        static constexpr double DEFAULT_SVD_THRESHOLD = 0.05;
 
-        ForceEstimation(ModelInterface::ConstPtr model, 
-                        double svd_threshold = DEFAULT_SVD_THRESHOLD);
-        
-        ForceTorqueSensor::ConstPtr add_link(std::string name, 
-                                             std::vector<int> dofs = {}, 
-                                             std::vector<std::string> chains = {});
-        
-        void update();
-        
-        void log(MatLogger::Ptr logger) const;
-        
-        
-    private:
-        
-        void compute_A_b();
-        void solve();
-        
-        struct TaskInfo
-        {
-            ForceTorqueSensor::Ptr sensor;
-            std::vector<int> dofs;
-            std::string link_name;
-            
-        };
-        
-        ModelInterface::ConstPtr _model;
-        
-        Eigen::MatrixXd _Jtot;
-        Eigen::MatrixXd _A;
-        Eigen::MatrixXd _Jtmp;
-        
-        Eigen::VectorXd _y, _tau, _g, _b, _sol;
-        
-        std::vector<TaskInfo> _tasks;
-        std::set<int> _meas_idx;
-        int _ndofs; 
-        
-        Eigen::JacobiSVD<Eigen::MatrixXd> _svd;
-        
-    };   
-    
-    
-    
+class ForceEstimation
+{
+
+public:
+
+    typedef std::shared_ptr<ForceEstimation> Ptr;
+
+    static constexpr double DEFAULT_SVD_THRESHOLD = 0.05;
+
+    /**
+         * @brief ForceEstimation constructor.
+         * @param model: shared pointer to ModelInterface; client code must keep this model up to date
+         * with respect to the robot state
+         * @param svd_threshold: threshold for solution regularization (close to singularities)
+         */
+    ForceEstimation(ModelInterface::ConstPtr model,
+                    double svd_threshold = DEFAULT_SVD_THRESHOLD);
+
+    /**
+    * @brief The add_link method adds one link to the list of estimated forces.
+    * @param name: the name of the link
+    * @param dofs: force indices to be estimated (e.g. {0,1,2} for just linear force)
+    * @param chains: chain names whose joints are used for the estimation (e.g. {"left_arm"})
+    * @return a shared pointer to a virtual force-torque sensor, which is updated during the
+    * call to update(); use getWrench() on it to retrieve the estimated wrench.
+    */
+    ForceTorqueSensor::ConstPtr add_link(std::string name,
+                                         std::vector<int> dofs = {},
+                                         std::vector<std::string> chains = {});
+
+    /**
+    * @brief update computes the estimation and updates all registered virtual FT-sensors
+    */
+    void update();
+
+    void log(MatLogger::Ptr logger) const;
+
+
+private:
+
+    void compute_A_b();
+    void solve();
+
+    struct TaskInfo
+    {
+        ForceTorqueSensor::Ptr sensor;
+        std::vector<int> dofs;
+        std::string link_name;
+
+    };
+
+    ModelInterface::ConstPtr _model;
+
+    Eigen::MatrixXd _Jtot;
+    Eigen::MatrixXd _A;
+    Eigen::MatrixXd _Jtmp;
+
+    Eigen::VectorXd _y, _tau, _g, _b, _sol;
+
+    std::vector<TaskInfo> _tasks;
+    std::set<int> _meas_idx;
+    int _ndofs;
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> _svd;
+
+};
+
+
+
 } } }
 
 
