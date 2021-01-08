3d91d2e1ad6910374f2aaf221b4403006cc111ba
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-01-08 17:41:41 +0100

    small fixes
    # Conflicts:
    #	examples/configs/coman_stack.yaml

--- src/opensot/OpenSotImpl.cpp
+++ src/opensot/OpenSotImpl.cpp
@@ -150,9 +150,6 @@ void OpenSotImpl::make_constraint_adapter(ConstraintDescription::Ptr constr_desc
 OpenSotImpl::OpenSotImpl(ProblemDescription ik_problem,
                          Context::Ptr context):
     CartesianInterfaceImpl(ik_problem, context),
-    _logger(XBot::MatLogger::getLogger(
-        context->params()->getLogPath() +
-        "/xbot_cartesian_opensot_log_" + std::to_string(rand()))),
     _vars({}),
     _force_space_references(false)
 {
@@ -440,7 +437,7 @@ bool OpenSotImpl::update(double time, double period)
 
 OpenSotImpl::~OpenSotImpl()
 {
-    _logger->flush();
+
 }
 
 
