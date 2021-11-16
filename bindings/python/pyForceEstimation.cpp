3b80f0c5e2638d2321a0d72162408d1883271512
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-11-16 12:29:35 +0100

    fixed momentum based binding

--- bindings/python/pyForceEstimation.cpp
+++ bindings/python/pyForceEstimation.cpp
@@ -15,7 +15,8 @@ PYBIND11_MODULE(pyest, m) {
         ;
 
     py::class_<ForceEstimationMomentumBased, ForceEstimation>(m, "ForceEstimationMomentumBased")
-        .def(py::init<XBot::ModelInterface::ConstPtr, double, double>())
+        .def(py::init<XBot::ModelInterface::ConstPtr, double, double, double>(), py::arg("model"), py::arg("rate"), py::arg("svd_th"), py::arg("obs_bw"))
         ;
+        
 }
 
