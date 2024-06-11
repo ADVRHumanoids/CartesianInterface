04bbd097f6daa4ee54a2857f119a121d8ce3bdfe
Arturo Laurenzi <arturo.laurenzi@iit.it>
2024-06-11 14:24:30 +0200

    take Affine3 from xbot2_interface bindings

--- bindings/python/pyci_all.py
+++ bindings/python/pyci_all.py
@@ -1,7 +1,7 @@
 from cartesian_interface import pyci
 from cartesian_interface import roscpp_utils as roscpp
 from cartesian_interface import pyest
-from cartesian_interface.affine3 import Affine3
+from xbot2_interface.pyaffine3 import Affine3
 from cartesian_interface.impedance import Impedance
 
 
