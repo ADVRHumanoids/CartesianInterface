c153cc52b39ef85b28db2bd4e5a34f9a9e259ae3
ngazzanelli <niccolo.gazzanelli@iit.it>
2025-02-25 12:34:24 +0100

    missing pybind11/stl.h include in pyForceEstimation

--- bindings/python/pyForceEstimation.cpp
+++ bindings/python/pyForceEstimation.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <pybind11/eigen.h>
 #include <estimation_utils/payload/force_estimation.h>
 #include <xbot2_interface/xbotinterface2.h>
